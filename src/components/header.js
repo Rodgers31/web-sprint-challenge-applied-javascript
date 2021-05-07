46459357f30e61f5ab07c81daf56ee304d0c6ad7
Rodgers Otieno <otienor@uindy.edu>
2021-05-07 18:36:06 -0400

    All test passing except one

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,23 @@
 const Header = (title, date, temp) => {
+
+  const titleHeader = document.createElement('div')
+  const titleDate = document.createElement('span')
+  const headTitle = document.createElement('h1')
+  const headTemp = document.createElement('span')
+  
+   titleHeader.classList.add('header')
+  titleDate.classList.add('date')
+  headTemp.classList.add('temp')
+  
+  headTitle.textContent = title
+  titleDate.textContent = date
+  headTemp.textContent = temp
+  
+   titleHeader.appendChild(titleDate)
+   titleHeader.appendChild(headTitle)
+   titleHeader.appendChild(headTemp)
+  
+  return  titleHeader
   // TASK 1
   // ---------------------
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
@@ -14,12 +33,17 @@ const Header = (title, date, temp) => {
 }
 
 const headerAppender = (selector) => {
+  const create = document.querySelector(selector);
+  create.appendChild(Header("We developer blog", "May 7", "Breaking News: Arsenal Fc loosing in europa league semifinals"));
+  
+}
+
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
-}
+
 
 export { Header, headerAppender }
